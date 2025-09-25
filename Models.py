5bc003ac07d7cdefed6bdee95c173af78e30e1ef
TKay25 <188647232+TKay25@users.noreply.github.com>
2025-09-25 15:44:55 +0200

    jkbkjb

--- Models.py
+++ Models.py
@@ -168,8 +168,11 @@ def landingpage():
     print(zwg_df)
     print(zwg_df)
 
-    zwg_df = zwg_df.set_index("tenor").T.reset_index()
-    usd_df = usd_df.set_index("tenor").T.reset_index()
+    zwg_df = zwg_df.set_index("tenor").T[tenor_order].reset_index()
+    usd_df = usd_df.set_index("tenor").T[tenor_order].reset_index()
+
+    tenor_order = ["<1m", "1m-2m", "2m-3m", "3m-6m", "6m-9m", "9m-12m", "1y-2y", "2y-3y", "3y-5y", "+5y"]
+
 
     print(zwg_df)
     print(zwg_df)
