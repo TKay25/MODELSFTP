d397f3a36ea3a42d10a5dbe0fd7d8697ff44c495
TKay25 <188647232+TKay25@users.noreply.github.com>
2025-09-26 15:10:51 +0200

    Update Models.py

--- Models.py
+++ Models.py
@@ -339,7 +339,7 @@ def landingpage():
     def fetch_table(table_name):
         """Fetch table into pandas DataFrame"""
         conn = psycopg2.connect(external_database_url)
-        query = f'SELECT * FROM "{table_name}" ORDER BY Tenor;'
+        query = f'SELECT * FROM "{table_name}";'
         df = pd.read_sql(query, conn)
         conn.close()
         return df
